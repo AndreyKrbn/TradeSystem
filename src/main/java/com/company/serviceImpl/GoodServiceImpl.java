e70a8e27a022b74f6ca92530d7798beeccfd8ded
Karabanov_Andrey <Andrey.krbn@gmail.com>
2019-08-20 16:50:08 +0300

    fix

---
+++ src/main/java/com/company/serviceImpl/GoodServiceImpl.java
@@ -0,0 +1,43 @@
+package com.company.serviceImpl;
+
+import com.company.GoodRepository;
+import com.company.GoodService;
+import com.company.model.Good;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+@Component
+public class GoodServiceImpl implements GoodService {
+
+    @Autowired
+    private GoodRepository goodRepository;
+
+
+    @Transactional(readOnly=true)
+    public List<Good> findAll() {
+        return Lists.newArrayList(goodRepository.findAll());
+    }
+
+
+    public Good findById(Long id) {
+        return goodRepository.findOne(id);
+    }
+
+
+    public Good save(Good good) {
+        return goodRepository.save(good);
+    }
+
+    @Autowired
+    public void setGoodRepository(GoodRepository goodRepository) {
+        this.goodRepository =goodRepository;
+}
+
+}
