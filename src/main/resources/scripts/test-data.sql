36373f462ba5b1c517b53a2508e76eb2f4e1edb3
Karabanov_Andrey <Andrey.krbn@gmail.com>
2019-08-21 14:58:50 +0300

    Relise

--- src/main/resources/scripts/test-data.sql
+++ src/main/resources/scripts/test-data.sql
@@ -1,3 +1,3 @@
-insert into good (name, cost, storage_quantity) values ('Фонарь', '5', '100');
-insert into good (name, cost, storage_quantity) values ('Акумулятор 16850', '1', '150');
-insert into good (name, cost, storage_quantity) values ('Зарядное устройство', '3', '250');
+insert into good (name, cost, storage_quantity) values ('Flashlight', '5', '100');
+insert into good (name, cost, storage_quantity) values ('Battery 16850', '1', '150');
+insert into good (name, cost, storage_quantity) values ('Charger', '3', '250');
